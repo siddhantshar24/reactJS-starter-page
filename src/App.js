09a1a6f633b0f15fc8de89a72e8f69b5e001c3d9
Siddhant Sharma <120168662+siddhantsharma24@users.noreply.github.com>
2022-12-31 21:12:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello this is my first react app</h1>
+      <h2>I am learning react from scratch</h2>
+    </div>
+  );
+}
+
+export default App;
